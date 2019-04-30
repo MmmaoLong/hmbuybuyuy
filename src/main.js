d124e885fad41006a77db1d25fc9bb166cb87f98
Your Name <you@example.com>
2019-04-30 15:37:22 +0800

    '首页渲染完成'

--- src/main.js
+++ src/main.js
@@ -1,27 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
-//导入路由
-import VueRouter from 'vue-router'
-Vue.use(VueRouter)
+// 导入饿了么ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
-//写组件
+
+
+// 导入路由
+import VueRouter from 'vue-router'
+// Vue.use一下
+Vue.use(VueRouter)
+// 写组件
 import index from './components/index.vue'
-//写规则
-const routes= [
+// 写规则
+const routes = [
   {
-    path:'/index',
-    component:index
+    path: '/index',
+    component: index
   }
 ]
-
-const router =new VueRouter({
+// 实例化路由对象
+const router = new VueRouter({
   routes
 })
+
+// 导入全局样式
+// 如果只是导入 不使用 可以省略 xxx from
+// import baseCss from './assets/statics/site/css/style.css'
 import './assets/statics/site/css/style.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 挂载到vue实例上
   router
 }).$mount('#app')
